27208debe66d7fd9e4d69ba40ef6a0540285c926
dceoy <dnarsil+github@gmail.com>
2024-02-25 00:17:13 +0900

    Specify default tags

--- envs/plt/provider.tf
+++ envs/plt/provider.tf
@@ -1,7 +1,19 @@
 provider "aws" {
   region = var.region
+  default_tags {
+    tags = {
+      SystemName = var.system_name
+      EnvType    = var.env_type
+    }
+  }
 }
 
 provider "awscc" {
   region = var.region
+  default_tags {
+    tags = {
+      SystemName = var.system_name
+      EnvType    = var.env_type
+    }
+  }
 }
