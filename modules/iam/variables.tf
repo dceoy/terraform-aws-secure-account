2d3a86a65a1789bc654f83bf30884dcaacdbb8cf
dceoy <dceoy@users.noreply.github.com>
2025-02-10 23:01:01 +0900

    Update default variables

--- modules/iam/variables.tf
+++ modules/iam/variables.tf
@@ -1,13 +1,11 @@
 variable "system_name" {
   description = "System name"
   type        = string
-  default     = "acc"
 }
 
 variable "env_type" {
   description = "Environment type"
   type        = string
-  default     = "plt"
 }
 
 variable "account_id" {
--- modules/securityhub/variables.tf
+++ modules/securityhub/variables.tf
@@ -1,13 +1,11 @@
 variable "system_name" {
   description = "System name"
   type        = string
-  default     = "acc"
 }
 
 variable "env_type" {
   description = "Environment type"
   type        = string
-  default     = "plt"
 }
 
 variable "sns_kms_key_arn" {
