b257b8d25a8bcbde9d8d6ac78ce2ea99071710f3
dceoy <1938249+dceoy@users.noreply.github.com>
2025-12-18 04:36:23 +0900

    Update modules/s3/outputs.tf

--- modules/s3/outputs.tf
+++ modules/s3/outputs.tf
@@ -3,9 +3,29 @@ output "awslogs_s3_bucket_id" {
   value       = aws_s3_bucket.awslogs.id
 }
 
+output "awslogs_s3_bucket_domain_name" {
+  description = "AWS service logs S3 bucket domain name"
+  value       = aws_s3_bucket.awslogs.bucket_domain_name
+}
+
+output "awslogs_s3_bucket_regional_domain_name" {
+  description = "AWS service logs S3 bucket regional domain name"
+  value       = aws_s3_bucket.awslogs.bucket_regional_domain_name
+}
+
 output "s3logs_s3_bucket_id" {
   description = "S3 server access logs S3 bucket ID"
   value       = length(aws_s3_bucket.s3logs) > 0 ? aws_s3_bucket.s3logs[0].id : null
+}
+
+output "s3logs_s3_bucket_domain_name" {
+  description = "S3 server access logs S3 bucket domain name"
+  value       = length(aws_s3_bucket.s3logs) > 0 ? aws_s3_bucket.s3logs[0].bucket_domain_name : null
+}
+
+output "s3logs_s3_bucket_regional_domain_name" {
+  description = "S3 server access logs S3 bucket regional domain name"
+  value       = length(aws_s3_bucket.s3logs) > 0 ? aws_s3_bucket.s3logs[0].bucket_regional_domain_name : null
 }
 
 output "s3_storage_lens_configuration_id" {
